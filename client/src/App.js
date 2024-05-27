6c3740b011b74d349ad513af53a81b169cc4e026
KacperKsiez <KacperKsiez@gmail.com>
2024-05-27 10:33:14 +0200

    git ignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [data, setData] = useState(null);
+
+	const download = async () => {
+		const downloadedData = await axios.get(`https://notatnik-server-drnciyj3m-kacperksiezs-projects.vercel.app/`);
+		setData(downloadedData.data);
+	};
+
+	useEffect(() => {
+		download();
+	}, []);
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
+	return <div>{data}</div>;
+};
 
 export default App;
